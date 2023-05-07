de07d736c6205c47743fa980187f36695102656b
Travis Reid Hopkins <trh.foss@protonmail.com>
2023-05-07 11:10:10 -0400

    build: Switched to Typescript
    Builds are now done in Typescript. I don't understand everything in the
    .tsconfig.json file, or how it connects to webpack, so it may be worth
    documenting the reason for each configuration option. This is especially
    important for differentiating development, testing, and production
    builds.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "./style.css"
 import printMe from "./print"
 
 const comp = () => {
-    const btn = document.createElement("button")
+    const btn: HTMLButtonElement = document.createElement("button")
     btn.innerHTML = "Click me and check the console!"
     btn.onclick = printMe
     return btn
