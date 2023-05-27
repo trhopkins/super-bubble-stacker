19c2f0afc9559c04d8003f96588bf7afa811b27b
Travis Reid Hopkins <trh.foss@protonmail.com>
2023-05-26 22:06:48 -0400

    style: added default eslint config
    ESLint now runs on all code in this repository with default settings.
    Next up, to fix indentation and trailing comma warnings.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: true,
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
